bf51cf8701002d01a596f2eec99e17316894caea
opilane <opilane@HP-CZC6297B82>
2022-09-13 11:44:04 +0300

    Add project files.

---
+++ MangAleksejev/Ese.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangAleksejev
+{
+
+    abstract class Ese : IUksus
+    {
+        public string nimetus;
+        public int punktidearv;
+    
+
+        public Ese(string nimetus, int punktudearv)//Klassis peab olema kahe parameetriga konstruktor, mille abil saab määrata nimetuse ja punktidearvu
+        {
+            this.nimetus = nimetus;
+            this.punktidearv = punktudearv;
+
+        }
+        public int punktudearv() //Meetod punktideArv tagastab punktide arvu.
+        {
+            return punktidearv;
+        }
+        public string info()//Meetod info tagastab selle eseme nimetuse.
+        {
+            return nimetus;
+        }
+    }
+}   
+
---
+++ MangAleksejev/Uksus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangAleksejev
+{
+    interface IUksus
+    {
+        int puhtrpunktidearv()//Klassis on privaatsed isendiväljad järgmise info jaoks: nimetus (String) ja punktide arv (int).
+        {
+            throw new NotImplementedException();
+        }
+        string info()
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+
+    
+}
+//1.Liides Üksus sisaldab
+//1. int-tüüpi parameetriteta meetodit punktideArv;
+//2.String - tüüpi parameetriteta meetodit info.
